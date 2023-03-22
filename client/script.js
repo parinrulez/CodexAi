d27845389cd361631963892f6314217b9480d5e6
parinrulez <sheth.parin05@gmail.com>
2023-03-22 16:27:47 +0530

    render new server deploy

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server => bot's response
 
-  const response = await fetch('https://codexai-4aus.onrender.com', {
+  const response = await fetch('https://codexai-eknm.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
